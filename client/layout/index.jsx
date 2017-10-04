338732a8244982261fe1299d13022a1e3b901029
Stéphane Thomas <email@stephanethomas.com>
2017-10-04 19:49:20 +0200

    Sidebar: Hide placeholder displayed when webpack build monitor chunk loads

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -171,7 +171,7 @@ Layout = React.createClass( {
 					isActive={ translator.isActivated() } />
 				{ this.renderPreview() }
 				{ config.isEnabled( 'happychat' ) && this.props.chatIsOpen && <AsyncLoad require="components/happychat" /> }
-				{ 'development' === process.env.NODE_ENV && <AsyncLoad require="components/webpack-build-monitor" /> }
+				{ 'development' === process.env.NODE_ENV && <AsyncLoad require="components/webpack-build-monitor" placeholder={ null } /> }
 				<AppBanner />
 			</div>
 		);
